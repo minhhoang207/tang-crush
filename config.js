cdce20122622c4e7ca2a40bd2a083eeabf29ccc8
minhhoang207 <86257510+minhhoang207@users.noreply.github.com>
2021-06-22 01:47:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'heyy Bianca Marie Gonzales!',
+    introDesc: `today is your bday😙😙 `,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    title: 'so ill give u 3 wishes 😙',
+    desc: 'well here iam what are ur other 2 wishes? ',
+    btnYes: 'u <33 ',
+    btnNo: 'hell no :3',
+    question:'cant choose no right hahaha 😌 now u belong to me haha... text love for next step😝 ',
+    btnReply: 'send me <3 ',
+    reply: 'love',
+    mess: 'just kidding 🤣. happy bday girl 😘😘',
+    messDesc: 'text me back if u like it.',
+    btnAccept: 'this so fk cool',
+    messLink: 'http://nmh207' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
